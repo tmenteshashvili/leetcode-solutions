28a8b9958e4f757bd4faee63533fcffd783c6606
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2025-01-11 19:14:06 +0100

    Add 383. Ransom note problemset

---
+++ 383 - Ransom Note.swift
@@ -0,0 +1,23 @@
+class Solution {
+  func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
+    if ransomNote.count > magazine.count { return false }
+    var ransomNoteFreq = [Int](repeating: 0, count: 26)
+    var magazineFreq = [Int](repeating: 0, count: 26)
+
+    for i in ransomNote {
+      ransomNoteFreq[Int(i.asciiValue!) - 97] += 1
+    }
+
+    for char in magazine {
+      magazineFreq[Int(char.asciiValue!) - 97] += 1
+    }
+
+    for i in 0..<ransomNoteFreq.count {
+      if ransomNoteFreq[i] > magazineFreq[i] {
+        return false
+      }
+    }
+
+    return true
+  }
+}

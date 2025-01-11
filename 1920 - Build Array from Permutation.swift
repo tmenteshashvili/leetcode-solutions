4f09c51ebe844646edc0ccf4c0010f53a79e3030
Tako Menteshashvili <64552231+tmenteshashvili@users.noreply.github.com>
2025-01-11 22:14:47 +0100

    Add 1920. Build Array from Permutation

---
+++ 1920 - Build Array from Permutation.swift
@@ -0,0 +1,13 @@
+// Problemset url: https://leetcode.com/problems/build-array-from-permutation/
+
+class Solution {
+  func buildArray(_ nums: [Int]) -> [Int] {
+    var ans = [Int](repeating: 0, count: nums.count)
+
+    for i in 0..<nums.count {
+      ans[i] = nums[nums[i]]
+
+    }
+    return ans
+  }
+}
